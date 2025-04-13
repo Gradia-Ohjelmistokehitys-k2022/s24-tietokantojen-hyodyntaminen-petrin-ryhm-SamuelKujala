60e353cbe53ba76868b193ae6bbd0662de5be086
SamuelKujala <Samuel.Kujala88@gmail.com>
2025-04-13 12:03:42 +0300

    Done 01_kirjasto

---
+++ 01_kirjasto/LibraryApp/Models/DataBaseRepository.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlTypes;
+
+namespace LibraryApp.Models
+{
+    public class DataBaseRepository
+    {
+        private string _connectionString;
+
+        public DataBaseRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public string IsDbConnectionEstablished()
+        {
+            
+            using var connection = new SqlConnection(_connectionString);
+
+            try
+            {
+                connection.Open();
+                return "Connection established!";
+            }
+            catch (SqlException ex)
+            {
+                throw;
+            }
+
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public void GetBooksFromFiveYears()
+        {
+
+            using var dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+
+            string query = "SELECT BookId, Title, ISBN, PublicationYear, AvailableCopies FROM Book Where PublicationYear >= Year(GetDate()) - 5";
+            using var command = new SqlCommand(query, dbConnection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Book book = new()
+                {
+                    Id = Convert.ToInt32(reader["BookId"]),
+                    Title = reader["Title"].ToString(),
+                    ISBN = reader["ISBN"].ToString(),
+                    PublicationYear = Convert.ToInt32(reader["PublicationYear"]),
+                    AvailableCopies = Convert.ToInt32(reader["AvailableCopies"].ToString())
+                };
+
+                Console.WriteLine($"{book.Id} - {book.Title} ISBN: {book.ISBN} ({book.PublicationYear})");
+            }
+        }
+
+        public void GetCUstomersCenterAge()
+        {
+            using var dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+
+            string query = "SELECT AVG(DATEDIFF(YEAR, syntymäaika, GETDATE())) as keski_ika FROM Member";
+            using var command = new SqlCommand(query, dbConnection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Member Member = new()
+                {
+                    keski_ika = Convert.ToInt32(reader["keski_ika"])
+                };
+
+                Console.WriteLine($"Asiakkaiden keski-ika: {Member.keski_ika}");
+            }
+        }
+
+        public void GetBookWitchContainsMost()
+        {
+            using var dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+
+            string query = "SELECT Top 1 Title, AvailableCopies FROM book Order by AvailableCopies desc";
+            using var command = new SqlCommand(query, dbConnection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Book book = new()
+                {
+                    Title = Convert.ToString(reader["Title"]),
+                    AvailableCopies = Convert.ToInt32(reader["AvailableCopies"])
+                };
+
+                Console.WriteLine($"{book.Title} - Available Copies: {book.AvailableCopies}");
+            }
+        }
+
+        public void GetMemberHowLoan()
+        {
+            using var dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+
+            string query = "SELECT Title, ISBN, AvailableCopies FROM Book WHERE AvailableCopies = (SELECT MAX(AvailableCopies) FROM Book)";
+            using var command = new SqlCommand(query, dbConnection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Book Book = new()
+                {
+                    Title = reader["Title"].ToString(),
+                    ISBN = reader["ISBN"].ToString()
+                };
+
+                Console.WriteLine($"Book name: {Book.Title}, Book ISBN: {Book.ISBN}");
+            }
+        }
+
+        public void GetThreeMostLoanBook()
+        {
+            using var dbConnection = new SqlConnection(_connectionString);
+            dbConnection.Open();
+
+            string query = "SELECT TOP 1 loan.BookID, book.Title, book.ISBN, book.PublicationYear, book.AvailableCopies FROM Book book JOIN Loan loan ON book.BookID = loan.BookID GROUP BY loan.BookID, book.Title, book.ISBN, book.PublicationYear, book.AvailableCopies ORDER BY COUNT(loan.BookID) DESC";
+            using var command = new SqlCommand(query, dbConnection);
+            using SqlDataReader reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Loan Loan = new()
+                {
+                    BookId = Convert.ToInt32(reader["BookID"])
+                };
+
+                Book book = new()
+                {
+                    Title = reader["Title"].ToString(),
+                    ISBN = reader["ISBN"].ToString(),
+                    PublicationYear = Convert.ToInt32(reader["PublicationYear"]),
+                    AvailableCopies = Convert.ToInt32(reader["AvailableCopies"])
+                };
+
+                Console.WriteLine($"Book ID: {Loan.BookId}, Book Name: {book.Title}, Book ISBN: {book.ISBN}, Book PublicationYear: {book.PublicationYear}, Book AvailableCopies: {book.AvailableCopies}");
+            }
+        }
+    }
+}
---
+++ 01_kirjasto/LibraryApp/Program.cs
@@ -0,0 +1,24 @@
+﻿using LibraryApp.Models;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        DataBaseRepository dataBaseRepository = new DataBaseRepository("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Library;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        dataBaseRepository.IsDbConnectionEstablished();
+
+        // Hae kaikki kirjat, jotka on julkaistu viiden vuoden sisällä. Tulosta kirjat konsoliin.
+        dataBaseRepository.GetBooksFromFiveYears();
+        // Hae kirjaston asiakkaiden keski-ikä. Tulosta keski-ikä konsoliin. Jos ikää ei ole, niin lisää syntymäaika tietokantaan.
+        dataBaseRepository.GetCUstomersCenterAge();
+        // Hae kirja, joita on eniten tarjolla kirjastossa. Tulosta kirjan nimi konsoliin.
+        dataBaseRepository.GetBookWitchContainsMost();
+        // Hae jäsenet, jotka lainasivat ainakin yhden kirjan kirjastosta. Tulosta jäsenen nimi ja kirjan ISBN konsoliin.
+        dataBaseRepository.GetMemberHowLoan();
+        // (Bonus) Hae kolmen lainatuimman kirjan kaikki tiedot konsoliin. Vihje! Tarvitset useamman Join-lauseen.
+        dataBaseRepository.GetThreeMostLoanBook();
+    }
+}
